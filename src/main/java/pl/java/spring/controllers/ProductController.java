f2cc3f7d47a3cf3acc503cfe497df4b88a1c9033
MagdalenaRosa <magdalena.joanna.rosa@gmail.com>
2023-04-16 08:43:25 +0200

    initial comit, getMapping , cONTROLLER

--- src/main/java/pl/java/spring/controllers/ProductController.java
+++ src/main/java/pl/java/spring/controllers/ProductController.java
@@ -3,17 +3,20 @@ package pl.java.spring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller // thx temu moge przejśc do tworzenia kontrolerów
 public class ProductController {
-    List<String>databases=List.of(
-            "iPhone11",
-            "iPhone12",
-            "iPhone13",
-            "iPhone14"
-    );
+    List<String>databases= new ArrayList<>();
+           ProductController(){
+            databases.add( "iPhone11");
+            databases.add( "iPhone12");
+            databases.add( "iPhone13");
+            databases.add( "iPhone14");
+    }
     @GetMapping ("/")// MA ODWOŁANIE DO GŁOWNEJ STRONY to jest nasz root
     public String ShowPrducts(Model model){
         // ten model to te bebechy springa 
@@ -30,5 +33,20 @@ public class ProductController {
 
 
 
+    }
+    @GetMapping("/saveProduct")
+    public String saveProduct(@RequestParam("productName") String productName, Model model){
+        // odbieranie danych
+//    public String saveProduct(@RequestParam String productName){ // odbieranie danych mozna uzunąc o ile nazwa jest taka sama
+        if(!databases.contains(productName)) {
+            databases.add(productName);
+            model.addAttribute("db", databases);
+        }
+        return "redirect:/";
+    }
+    @GetMapping("/removeProduct")
+    public String removeProduct(@RequestParam("productName") String productName){
+        databases.removeIf(s -> s.equals(productName));
+        return "redirect:/";
     }
 }
